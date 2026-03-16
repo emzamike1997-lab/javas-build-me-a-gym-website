676af0c02aa932f4d1f75d57d1dea29f8232ef5a
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 11:53:07 +0100

    JAVAS: Add src/components/Services.js

---
+++ src/components/Services.js
@@ -0,0 +1,21 @@
+```javascript
+import React from 'react';
+import './Services.css';
+
+function Services() {
+    return (
+        <section className="services">
+            <h1 className="services-title">Our Services</h1>
+            <ul className="services-list">
+                <li className="services-item">Personal Training</li>
+                <li className="services-item">Group Fitness Classes</li>
+                <li className="services-item">Nutrition Counseling</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Services;
+```
+
+###
