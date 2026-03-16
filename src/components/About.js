eab8c4616756e7718282d9aca80b1bccf2550066
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 11:53:06 +0100

    JAVAS: Add src/components/About.js

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+```javascript
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <section className="about">
+            <h1 className="about-title">About Us</h1>
+            <p className="about-description">
+                Our gym is owned and operated by a team of experienced fitness professionals who are passionate about helping others achieve their fitness goals.
+            </p>
+        </section>
+    );
+}
+
+export default About;
+```
+
+###
