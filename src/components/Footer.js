b12d60dd40770009a4c19bcdcd907b70a288febb
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 11:53:10 +0100

    JAVAS: Add src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+```javascript
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">
+                &copy; 2024 Gym Website. All rights reserved.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
+```
+
+###
