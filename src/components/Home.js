864fceee91861f5c40578ac8c2800f15c77b04bb
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 11:53:05 +0100

    JAVAS: Add src/components/Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+```javascript
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <section className="home">
+            <h1 className="home-title">Welcome to our gym</h1>
+            <p className="home-description">
+                Our gym is dedicated to providing a safe and supportive environment for individuals to achieve their fitness goals.
+            </p>
+            <button className="home-cta">Join Now</button>
+        </section>
+    );
+}
+
+export default Home;
+```
+
+###
